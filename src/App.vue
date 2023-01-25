bdf04faf195600b4ef5e6de2ed39758c88f24770
Максим <vokanson@gmail.com>
2023-01-25 07:49:11 +0600

    new

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,43 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+<h1><strong>Поиск фильма по названию</strong></h1>
+  <input type="text" placeholder="Введите название фильма"
+         v-on:input="inputChangeHandler">
+  <button v-on:click="search">Поиск</button>
+  <ul class="list" >
+    <li class="list-item" v-for="f in dat.Search"  >
+      {{f.Title}} {{f.Year}} <img :src=f.Poster>
+    </li>
+  </ul>
+  <p>{{aboutFilm}}</p>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+export default {
+  data() {
+    return {
 
-nav {
-  padding: 30px;
+      nameFilm: "",
+      aboutFilm: "",
+      dat: []
+    }
+  },
+  methods: {
+    search:
+        async function () {
+          const films = this.inputValue
+          const res = await fetch(`http://www.omdbapi.com/?apikey=922db138&s=${films}`)
+          this.dat = await res.json()
+          this.nameFilm = this.inputValue
+          // this.aboutFilm = dat.Plot
+          console.log(this.dat)
+        },
+    inputChangeHandler(event) {
+      this.inputValue = event.target.value
+    }
+  }
 }
+</script>
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+<style scoped>
 
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
